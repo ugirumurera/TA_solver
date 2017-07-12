0482cae796d056bf3f105793acef66ad186ba9ff
Juliette Ugirumurera <julymurera@gmail.com>
2017-07-12 14:14:43 -0700

    Changes to Entry_Point_BeATS.java

--- java/py4jbeats/src/main/java/Entry_Point_BeATS.java
+++ java/py4jbeats/src/main/java/Entry_Point_BeATS.java
@@ -2,24 +2,30 @@ import py4j.GatewayServer;
 
 public class Entry_Point_BeATS {
 
-    private api.API api;
+	private api.API api;
 
-    public Entry_Point_BeATS() {
-        api = new api.API();
-    }
+	public Entry_Point_BeATS() {
+		api = new api.API();
+	}
 
-    public api.API get_BeATS_API(){
-    	return api;
-    }
+	public api.API get_BeATS_API(){
+		return api;
+	}
 
-    public static void main(String[] args) {
-        GatewayServer gatewayServer;
-        if(1==args.length)
-            gatewayServer = new GatewayServer(new Entry_Point_BeATS(),Integer.parseInt(args[0]));
-        else
-            gatewayServer = new GatewayServer(new Entry_Point_BeATS());
-        gatewayServer.start();
-        System.out.println("Gateway Server Started on port " + gatewayServer.getPort());
-    }
-    
+	public static void main(String[] args) {
+		try{
+			GatewayServer gatewayServer;
+			if(1==args.length)
+				gatewayServer = new GatewayServer(new Entry_Point_BeATS(),Integer.parseInt(args[0]));
+			else
+				gatewayServer = new GatewayServer(new Entry_Point_BeATS());
+			gatewayServer.start();
+			System.out.println("Gateway Server Started on port " + gatewayServer.getPort());
+			
+		} catch((Py4JNetworkException e){
+			System.out.println("Could not initialize Java Gateway");
+			System.exit(1);
+		}
+	}
+
 }
