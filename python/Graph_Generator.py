7d728b1004c88fa47ccd5d5a0e3196a3cbcaeecf
Gabriel Gomes <gomes@path.berkeley.edu>
2017-11-29 10:30:13 -0800

    start write to xml in Graph_Generator

--- python/Graph_Generator.py
+++ python/Graph_Generator.py
@@ -1,8 +1,13 @@
 from igraph import *
+from xml.etree.ElementTree import ElementTree
+from xml.etree.ElementTree import Element
+import xml.etree.ElementTree as etree
 import csv
-import numpy as np
+# import numpy as np
 import itertools
 import random
+from xml.dom import minidom
+
 
 #Function used to determine all paths between ods
 def find_all_paths(graph, start, end):
@@ -53,32 +58,51 @@ def translate_paths(g, paths):
         new_paths.append(trans_path)
     return new_paths
 
-def main():
-    # Size of grid
-    graph_size = 5  # grid size, leads to a grid of graph_size*graph_size nodes
-    num_nodes = graph_size*graph_size   # Number of nodes in the graph
-    num_ods = num_nodes/2    # Number of od, have to be less that 1/2 number of nodes
-    scaling = 1000  # number used to scale the resulting grid graph
-    max_length = 6  # Maximum number of nodes in paths returned
-    paths_per_od = 5    # Number of paths saved per OD
+def generate_graph_and_paths(graph_size, scaling, num_nodes, num_ods, max_length, paths_per_od):
 
-    g = Graph.Lattice([graph_size, graph_size], circular=False, directed = True)
-
+    graph = Graph.Lattice([graph_size, graph_size], circular=False, directed = True)
 
     # Get node ids and positions
-    layout = g.layout("kk")     # This fixes the coordinates of the nodes
+    layout = graph.layout("kk")     # This fixes the coordinates of the nodes
     layout.scale(scaling)
 
     coordinates = layout.coords    # Coordinates of the nodes
-    g.vs["Coordinates"] = coordinates
+    graph.vs["Coordinates"] = coordinates
 
+    # Generate od pairs
+    pairs = list(itertools.combinations(range(num_nodes), 2))   # Generates the pairs
+    random.shuffle(pairs)   # shuffles the od pairs to allow for variability
+    ods = pairs[0:num_ods]
+
+    # Generate the paths between the od pairs
+    all_paths = {}
+    for o in ods:
+        #Find all paths between origin and destination that have at most max_length edges
+        paths = find_all_paths_len(graph,o[0],o[1],maxlen=max_length)
+
+        #If we could not get paths_per_od paths between the od, double the max_length value
+        if len(paths) < paths_per_od:
+            paths = find_all_paths_len(graph,o[0],o[1],maxlen=max_length*2)
+
+        #Sort paths by length so that the shortest are first
+        paths.sort(key=len)
+        new_paths = translate_paths(graph, paths[0:paths_per_od])
+
+        all_paths[o] = new_paths
+
+    return graph, all_paths
+
+def get_node_for_links(begin_node, end_node):
+    return 0
+
+def write_to_csv(graph, all_paths):
 
     # Print vertices and their coordinates
     f = open('node_info.csv', 'wb')
     writer = csv.writer(f)
     node_info_title = ["Node id", "x coord", "y coord"]
     writer.writerow(node_info_title)
-    for v in g.vs:
+    for v in graph.vs:
         row = [v.index,v["Coordinates"][0],v["Coordinates"][1]]
         writer.writerow(row)
 
@@ -87,34 +111,128 @@ def main():
     writer = csv.writer(f)
     edge_info_title = ["Link id", "source node", "end node"]
     writer.writerow(edge_info_title)
-    for e in g.es:
+    for e in graph.es:
         row = [e.index,e.source, e.target]
         writer.writerow(row)
 
-    # Generate od pairs
-    pairs = list(itertools.combinations(range(num_nodes), 2))   # Generates the pairs
-    random.shuffle(pairs)   # shuffles the od pairs to allow for variability
-    ods = pairs[0:num_ods]
-
-    # Generate the paths between the od pairs and save them to csv file
+    # Write paths
     f = open('paths_info.csv', 'wb')
     writer = csv.writer(f)
-    for o in ods:
-        #Find all paths between origin and destination that have at most max_length edges
-        paths = find_all_paths_len(g,o[0],o[1],maxlen=max_length)
+    for o, path in all_paths.iteritems():
+        writer.writerows(path)
 
-        #If we could not get paths_per_od paths between the od, double the max_length value
-        if len(paths) < paths_per_od:
-            paths = find_all_paths_len(g,o[0],o[1],maxlen=max_length*2)
+# generate an xml tree 
+def write_to_xml(graph,all_paths):
 
-        #Sort paths by length so that the shortest are first
-        paths.sort(key=len)
-        new_paths = translate_paths(g, paths[0:paths_per_od])
-        writer.writerows(new_paths)
+    # initialize xml
+    xscenario = Element('scenario')
+    tree = ElementTree(xscenario)
+
+    # network ---------------------
+    xnetwork = Element('network')
+    xscenario.append(xnetwork)
+
+    # nodes ........................
+    xnodes = Element('nodes')
+    xnetwork.append(xnodes)
+    for v in graph.vs:
+        xnode = Element('node')
+        xnodes.append(xnode)
+        xnode.set('id', str(v.index))
+        xnode.set('x', str(v["Coordinates"][0]))
+        xnode.set('y', str(v["Coordinates"][1]))
+
+    # links .........................
+    xlinks = Element('links')
+    xnetwork.append(xlinks)
+    for v in graph.vs:
+        xlink = Element('link')
+        xlinks.append(xlink)
+        xlink.set('id', str(v.index))
+        xlink.set('length', str(100))   # length in meters --- FIX THIS
+        xlink.set('full_lanes', str(1)) # number of lanes --- FIX THIS
+        xlink.set('start_node_id', str(v["Coordinates"][0]))
+        xlink.set('end_node_id', str(v["Coordinates"][1]))
+        xlink.set('roadparam', "0" )
+
+    # road params .......................
+    xroadparams = Element('roadparams')
+    xnetwork.append(xroadparams)
+    xroadparam = Element('roadparam')
+    xroadparams.append(xroadparam)
+    xroadparam.set('id', "0")
+    xroadparam.set('name', "Standard link")
+    xroadparam.set('capacity', "2000" )     # veh/hr/lane
+    xroadparam.set('speed', "60" )          # km/hr
+    xroadparam.set('jam_density', "100" )   # veh/km/lane
+
+    # road connections .......................
+    # add only the road connections needed for the paths
+    xroadconnections = Element('roadconnections')
+    xnetwork.append(xroadconnections)
+    road_connection_map = {}
+    for od, paths in all_paths.iteritems():
+        print od
+
+        for path in paths:
+
+            for i in range(len(path)-1):
+                up_link = graph.es[path[i]]
+                dn_link = graph.es[path[i+1]]
+
+                if up_link.target != dn_link.source:
+                    print 'ERROR!!!!!', up_link.target, dn_link.source
+
+                node_id = up_link.target
+                if node_id in road_connection_map:
+                    # ignore if it is already present
+                    if len([item for item in road_connection_map[node_id] if item[0] == path[i] and item[0] == path[i+1]]) == 0:
+                        road_connection_map[node_id].append((path[i], path[i+1]))
+                else:
+                    road_connection_map[node_id] = [(path[i], path[i+1])]
+
+    print road_connection_map
+
+    # write to file
+    # with open(('scenario.xml'), 'w') as f:
+    #     f.write(minidom.parseString(etree.tostring(xscenario)).toprettyxml(indent="\t"))
+
+def this_test_fails(graph, all_paths):
+
+    for od, paths in all_paths.iteritems():
+
+        for path in paths:
+
+            for i in range(len(path)-1):
+                up_link = graph.es[path[i]]
+                dn_link = graph.es[path[i+1]]
+
+                if up_link.target != dn_link.source:
+                    print 'ERROR!!!!!', up_link.target, dn_link.source
+
+
+def main():
+
+    # user definitions
+    graph_size = 5  # grid size, leads to a grid of graph_size*graph_size nodes
+    scaling = 1000  # number used to scale the resulting grid graph
+    max_length = 6  # Maximum number of nodes in paths returned
+    paths_per_od = 5    # Number of paths saved per OD
+
+    num_nodes = graph_size*graph_size   # Number of nodes in the graph
+    num_ods = num_nodes/2    # Number of od, have to be less that 1/2 number of nodes
+
+    graph, all_paths = generate_graph_and_paths(graph_size, scaling, num_nodes, num_ods, max_length, paths_per_od)
+
+    this_test_fails(graph, all_paths)
+
+    # write_to_csv(graph,all_paths)
+
+    # write_to_xml(graph,all_paths)
 
     # Plot the resulting graph
-    g.vs["ID"] = g.vs.indices
-    g.vs["label"] = g.vs["ID"]  # We label the nodes by the ID
-    plot(g, layout = layout)    # Plotting the graph
+    # graph.vs["ID"] = graph.vs.indices
+    # graph.vs["label"] = graph.vs["ID"]      # We label the nodes by the ID
+    # plot(graph, layout = layout)            # Plotting the graph
 
 if __name__ == "__main__": main()
