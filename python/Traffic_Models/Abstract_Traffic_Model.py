fc2ecf2eb4a241552ab25ae57914bb5f9bda93ef
Juliette Ugirumurera <julymurera@gmail.com>
2018-02-05 16:42:57 -0800

    Added Back Traffic Model Folder

---
+++ python/Traffic_Models/Abstract_Traffic_Model.py
@@ -0,0 +1,62 @@
+#This is the abstract base class for all Traffic models
+#Each subclass has to implement all abstract method of this class
+
+from abc import ABCMeta, abstractmethod
+from py4j.java_gateway import JavaGateway,GatewayParameters
+
+class Abstract_Traffic_Model_class:
+    __metaclass__ = ABCMeta
+
+    # The Traffic Class initiates connection to Beats object
+    def __init__(self, beats_api):
+        self.beats_api = beats_api
+
+
+    # Validate the scenario loaded from the configuration file.
+    # Returns: A boolean. True if the scenario is valid, False otherwise.
+    # This is optional to implement
+    def Validate_Configfile(self):
+        return True
+
+    '''
+    Map from path demands to link states
+
+    Arguments:
+    A Demand_Assignment object, demand_assignments
+        This object stores a dictionary of the form [path_id, commodity_id] : [demand_t1, demand_t2,...]
+        This is to be initialized in the solver algorithm by user and shows the demand per path, per commodity and per
+        per time step.
+    initial_state: An State Trajectory object that specifies the initial state of links in terms of flow, number of vehicles
+                  and queue per link (Ignore in the case of static model)
+    dt: simulation time step in seconds. (Ignore in the case of a static model)
+    T: simulation time horizon in second. (Ignore in the case of a static model)
+
+    Returns: A State_Trajectory object, which stores a dictionary of the form [link_id, commodity_id] : [traffic_state_t1, traffic_state_t2,...]
+    Each Traffic_State object can have:
+        flows: corresponds to the average flow on link_id(i) of commodity(j) over time interval k (ie from t=k*dt to t=(k+1)*dt).
+            Units: vehicles/hour
+        vehicles: corresponds to the total number of vehicles on link_id(i) of commodity(j) at time instant k (ie t=k*dt)
+            Units: vehicles
+        queue: corresponds to the number of vehicles in link_id(i) of commodity(j) that are in a queue at time instant k (ie t=k*dt).
+            Units: vehicles
+
+    Assumtpions on the output:
+    + link_ids covers all links with non-zero output flow and state.
+    + commodity_ids covers all commodities with non-zero demand
+    + Assume that time steps are numbered from 0,1,2,...
+
+    '''
+    @abstractmethod
+    def Run_Model(self, demand_assignments, initial_state, T):
+        pass
+
+    # Gradient of the model
+    # Returns the variation of the path costs to small perturbations in the path demands,
+    # for a given level of path demands.
+    # The input and output format is similar to that of Run_Model, except that on the output
+    # side, the values should be interpreted as variations.
+    # Implementation of this function is optional. It enables gradient-based algorithms, however,
+    # if no gradient can be provided, then return None.
+    def Model_Gradient(self, demand_assignments, initial_state, T):
+        return None
+
---
+++ python/Traffic_Models/MN_Model.py
@@ -0,0 +1,121 @@
+#Static Traffic Model, assuming the demand is fixed
+
+from __future__ import division
+from Abstract_Traffic_Model import Abstract_Traffic_Model_class
+from Data_Types.State_Trajectory_Class import State_Trajectory_class
+from Traffic_States.MN_Traffic_State import MN_Traffic_State_class
+import numpy as np
+
+class MN_Model_Class(Abstract_Traffic_Model_class):
+    #Configfile is needed to initialize the model's scenario via beats_api
+    def __init__(self, beats_api, gateway):
+        self.gateway = gateway
+        self.model_type = 'mn'     #Indicates that this is a static model
+        Abstract_Traffic_Model_class.__init__(self, beats_api)
+
+    # def Validate_Configfile(self):
+    #     # If the configfile indicates varying demand, return with an error
+    #     # We first want to check the configfile to make sure it is in correct format
+    #     demand_array = self.beats_api.get_demands()
+    #     valid = True
+    #     i = 0 # index in the demand_array matrix
+    #     while i < demand_array.__len__() and valid:
+    #         if demand_array[i].getProfile().num_values() > 1:
+    #             return False
+    #         i = i + 1
+    #     return True
+
+    # Overides the Run_Model function in the abstract class
+    # Returns an array of link states where each entry indicates the flow per link, per commodity and per time step
+    def Run_Model(self, demand_assignment, initial_state=None, time_horizon=None,Vectorize = None):
+        start_time = 0.0
+        #comm_id = 1L
+        comm_list = demand_assignment.get_commodity_list()
+        path_cost_dt = float(demand_assignment.get_dt())
+        demand_dt = float(demand_assignment.get_dt())
+
+        api = self.beats_api
+
+        # Clear the path requests .............
+        api.clear_output_requests()
+
+        # request link veh output ..............
+        java_array = self.gateway.jvm.java.util.ArrayList()
+        for d in demand_assignment.get_list_of_links():
+            java_array.add(d)
+
+        #request flow on links for all commodities
+        for comm_id in comm_list:
+            api.request_links_flow(comm_id, java_array, path_cost_dt)
+
+        # clear demands in beats ................
+        api.clear_all_demands()
+
+        # # send demand assignment to beats ............
+        for path_comm, demand_list in demand_assignment.get_all_demands().iteritems():
+            path_id = path_comm[0]
+            comm_id = path_comm[1]
+            java_array = self.gateway.jvm.java.util.ArrayList()
+            for d in demand_list:
+                java_array.add(float(d))
+
+            api.set_demand_on_path_in_vph(path_id, comm_id, start_time, demand_dt, java_array)
+
+        # run BeATS .................
+        # api.set_random_seed(1)
+        api.run(float(start_time), float(time_horizon))
+
+        # cycle through beats outputs
+        sampling_dt = demand_assignment.get_dt()
+        num_steps = int(time_horizon/sampling_dt)
+
+        # We do this when vectorize it true, that we want to vectorize BPR
+        if Vectorize:
+            list_of_link_ids = self.beats_api.get_link_ids()
+            num_steps = demand_assignment.get_num_time_step()
+
+            # create a matrix to hold the states of all the links for num_steps
+            link_states = np.zeros((len(list_of_link_ids), num_steps))
+
+            for output in api.get_output_data():
+                for link_id in output.get_link_ids():
+                    #link_states.add_linkId(link_id)
+                    capacity_vph = self.beats_api.get_link_with_id(link_id).get_capacity_vps()*3600
+                    #volume = output.get_profile_for_linkid(link_id)
+
+                    #To be added once we have needed function
+                    #link_states[link_id,:] = flow_per_link
+                    #flow_per_link = np.divide(volume,capacity_vph)
+                    for i in range(num_steps):
+                        link_states[link_id,i] = output.get_flow_vph_for_linkid_timestep(link_id,i)/capacity_vph
+
+        else:
+
+            link_states = State_Trajectory_class(list([]),
+                                             list(self.beats_api.get_commodity_ids()), num_steps, sampling_dt)
+
+            for output in api.get_output_data():
+                for link_id in output.get_link_ids():
+                    link_states.add_linkId(link_id)
+                    comm = output.get_commodity_id()
+                    capacity_vph = self.beats_api.get_link_with_id(link_id).get_capacity_vps()*3600
+
+                    for i in range(num_steps):
+                        state = MN_Traffic_State_class()
+                        volume = output.get_flow_vph_for_linkid_timestep(link_id,i)
+                        state.set_state_parameters(volume, capacity_vph)
+                        link_states.set_state_on_link_comm_time(link_id, comm, i, state)
+
+            #print "\n"
+            #link_states.print_all()
+
+        return link_states
+
+    def get_total_demand(self):
+        demands = self.beats_api.get_demands()
+        total_demand = 0
+
+        for demand in demands:
+            total_demand = total_demand + demand.getProfile().get_value(0)*3600
+
+        return total_demand
