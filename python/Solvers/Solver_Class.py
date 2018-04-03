e7f6e691de8be1635799bdcc52476714ea5febb4
Juliette Ugirumurera <julymurera@gmail.com>
2018-04-03 16:54:12 -0700

    Changes in Solver class to use OD_Matrix

--- python/Solvers/Solver_Class.py
+++ python/Solvers/Solver_Class.py
@@ -216,7 +216,7 @@ class Solver_class():
         # We first start by initializing an initial solution/ demand assignment
         if ods == None:
             num_steps = T/sampling_dt
-            od_temp = list(self.model_manager.get_OD_Matrix(num_steps, sampling_dt))
+            od_temp = list(self.model_manager.get_OD_Matrix(num_steps, sampling_dt).get_all_ods().values())
         else: od_temp = ods
 
         od = np.asarray(sorted(od_temp, key=lambda h: (h.get_origin(), h.get_destination())))
@@ -299,34 +299,38 @@ class Solver_class():
         # This function receives the solution assignment and the corresponding path_costs and returns the distance to Nash
     # calculated as of the summation of the excess travel cost for flows on paths compared to the travel cost on the
     # shortest paths for an origin-destination pair
-    def distance_to_Nash(self, sol_assignment, path_costs, sampling_dt):
+    def distance_to_Nash(self, sol_assignment, path_costs, sampling_dt, OD_Matrix = None):
         num_steps = sol_assignment.get_num_time_step()
-        od = self.model_manager.beats_api.get_od_info()
+        if OD_Matrix == None:
+            OD_Matrix = self.model_manager.get_OD_Matrix(num_steps, sampling_dt)
+
+        od = OD_Matrix.get_all_ods().values()
         dist_to_Nash = 0
 
         # For each OD, we are going to move its demand to the shortest path at current iteration
         for o in od:
             min_cost = 0
-            comm_id = o.get_commodity_id()
+            comm_id = o.get_comm_id()
+            od_path_list = o.get_path_list()
 
             for i in range(num_steps):
-                num_paths = len(o.get_subnetworks())
+                num_paths = len(od_path_list.keys())
                 min_path_id = -1
                 cost_on_paths = np.zeros(num_paths)
                 demand_on_paths = np.zeros(num_paths)
                 j = 0   # index in cost_on_paths and demand_on_paths arrays
 
-                for path in o.get_subnetworks():
+                for path_id in od_path_list.keys():
                     if min_path_id == -1:
-                        min_path_id = path.getId()
+                        min_path_id = path_id
                         min_cost = path_costs.get_cost_at_path_comm_time(min_path_id, comm_id, i)
-                    elif min_cost > path_costs.get_cost_at_path_comm_time(path.getId(), comm_id, i):
-                        min_path_id = path.getId()
+                    elif min_cost > path_costs.get_cost_at_path_comm_time(path_id, comm_id, i):
+                        min_path_id = path_id
                         min_cost = path_costs.get_cost_at_path_comm_time(min_path_id, comm_id, i)
 
                     # Collecting the costs and demands on paths for od o
-                    cost_on_paths[j] = path_costs.get_cost_at_path_comm_time(path.getId(), comm_id, i)
-                    demand_on_paths[j] = sol_assignment.get_demand_at_path_comm_time(path.getId(), comm_id, i)
+                    cost_on_paths[j] = path_costs.get_cost_at_path_comm_time(path_id, comm_id, i)
+                    demand_on_paths[j] = sol_assignment.get_demand_at_path_comm_time(path_id, comm_id, i)
                     j += 1
 
                 # Adding the excess travel cost to the distance from Nash
